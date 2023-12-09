ee3a8a75affca1d20f81398b54ed4dc35d853542
alocode19 <johnpatrick.alo@carsu.edu.ph>
2023-12-10 01:13:36 +0800

    Added Seeder and Api

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CarouselItemsController;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------- 
 | API Routes
 |--------------------------------------------------------------------------
 |
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/carousel', [CarouselItemsController::class, 'index']);
