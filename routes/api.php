9a75c8fb21ea72ec5763490fbe70cc00de5b3ae9
alocode19 <johnpatrick.alo@carsu.edu.ph>
2023-12-21 04:49:44 +0800

    Added User specifi update image and profile information show

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\CarouselItemsController;
 
 
@@ -18,10 +19,11 @@ use App\Http\Controllers\Api\CarouselItemsController;
 |
 */
 //Public APIs
-
+Route::controller(AuthController::class)->group(function (){
     Route::post('/login',[AuthController::class,'login'])->name('user.login');
-    Route::post('/user', [UserController::class,'store'])->name('user.store');
+    Route::post('/user',                 'store')->name('user.store');
 
+});
 
  
 //Private APIs  
@@ -42,7 +44,12 @@ use App\Http\Controllers\Api\CarouselItemsController;
         Route::put('/user/{id}',            'update')->name('user.update');
         Route::put('/user/email/{id}',      'email')->name('user.email');
         Route::put('/user/password/{id}',   'password')->name('user.password');
+        Route::put('/user/image/{id}',      'image')->name('user.image');
         Route::delete('/user/{id}',         'destroy');
 
     });
+//User Specific API
+    Route::get('/profile/show',            [ProfileController::class, 'show']);
+    Route::put('/profile/image',            [ProfileController::class, 'image'])->name('profile.image');
  });
+  
